d71e34bb83991ee6f141a62c9c11c7f00fc2f7ae
Tanish-Arora-01 <tanisharora1105@gmail.com>
2026-02-15 15:44:55 +0530

    performance improvement

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Skills from "./components/Skills";
 
 function App() {
   return (
-    <div className="fixed">
+    <div className="fixed inset-0 overflow-y-auto">
       <Navbar />
       <Hero />
       <About />

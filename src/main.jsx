cf966e0c3884b01f34f4ecc60c4579a29e68ee6f
Tanish-Arora-01 <tanisharora1105@gmail.com>
2026-02-15 15:15:54 +0530

    performance improvement

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <div className="min-h-[100dvh] bg-slate-950 text-white">
+      <App />
+    </div>
   </StrictMode>,
-)
+);
